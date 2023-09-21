0112024435f613d48158c85c14ee0c0e97802638
kevin1010607 <kevin.1010607@gmail.com>
2023-09-21 18:04:39 +0800

    add assignment1 code

---
+++ assignment1/assignment1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#define N 100
+
+int main() {
+    // Define string
+    char buffer[N];
+    int start_offset = 14;
+    char old_str[N] = "student.";
+    char new_str[N] = "NTHU student.";
+    int len_old_str = strlen(old_str);
+    int len_new_str = strlen(new_str);
+
+    // Open the file
+    int file_id = open("sample.txt", O_RDWR);
+    if(file_id == -1) {
+        printf("Failed to open the file.");
+        return 1;
+    }
+
+    // Use lseek and read to print “student.” in the command line.
+    lseek(file_id, start_offset, SEEK_SET);
+    read(file_id, buffer, len_old_str * sizeof(char));
+    buffer[len_old_str] = '\0';
+    printf("%s\n", buffer);
+
+    // Use lseek and write to replace “student.” with “NTHU student.” and
+    // print the whole sentence in the file in the command line.
+    lseek(file_id, start_offset, SEEK_SET);
+    write(file_id, new_str, len_new_str);
+    lseek(file_id, 0, SEEK_SET);
+    ssize_t bytes_read = read(file_id, buffer, sizeof(buffer));
+    if(bytes_read == -1) {
+        printf("Failed to read the file.");
+        return 1;
+    }
+    buffer[bytes_read] = '\0';
+    printf("%s\n", buffer);
+
+    // Close the file
+    close(file_id);
+
+    return 0;
+}
+
