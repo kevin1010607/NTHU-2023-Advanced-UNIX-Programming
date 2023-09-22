76ca8b32f4e19f8f9823fb86362f06ed047992ad
kevin1010607 <kevin.1010607@gmail.com>
2023-09-22 19:21:30 +0800

    add new line in the end

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -14,23 +14,25 @@ int main() {
     int len_old_str = strlen(old_str);
     int len_new_str = strlen(new_str);
 
-    // Open the file
+    // Open the file using O_RDWR flag
     int file_id = open("sample.txt", O_RDWR);
     if(file_id == -1) {
         printf("Failed to open the file.");
         return 1;
     }
 
-    // Use lseek and read to print “student.” in the command line.
+    // Use lseek and read to print “student.” in the command line
     lseek(file_id, start_offset, SEEK_SET);
     read(file_id, buffer, len_old_str * sizeof(char));
     buffer[len_old_str] = '\0';
     printf("%s\n", buffer);
 
-    // Use lseek and write to replace “student.” with “NTHU student.” and
-    // print the whole sentence in the file in the command line.
+    // Use lseek and write to replace “student.” with “NTHU student.”
     lseek(file_id, start_offset, SEEK_SET);
     write(file_id, new_str, len_new_str);
+    write(file_id, "\n", 1); // add a "\n" in the end.
+
+    // Print the whole sentence in the file in the command line
     lseek(file_id, 0, SEEK_SET);
     ssize_t bytes_read = read(file_id, buffer, sizeof(buffer));
     if(bytes_read == -1) {
@@ -38,7 +40,7 @@ int main() {
         return 1;
     }
     buffer[bytes_read] = '\0';
-    printf("%s\n", buffer);
+    printf("%s", buffer);
 
     // Close the file
     close(file_id);
